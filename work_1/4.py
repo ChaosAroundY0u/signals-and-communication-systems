625f4d117816d8539eac84b56e2fa24032f745b1
ChaosAroundY0u <152779643+ChaosAroundY0u@users.noreply.github.com>
2024-12-04 22:23:07 +0300

    Update 4.py - new QPSK algorithm

--- work_1/4.py
+++ work_1/4.py
@@ -1,28 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 64 # amount of subcarriers
-symbols_count = 5
-bits_count = N * symbols_count * 2 #QPSK => *2
-signal_to_noise_ratio = 15
-bits = np.random.randint(0, 2, bits_count)
-
-symbols = np.zeros((symbols_count, N), dtype = complex)
-
-for i in range(symbols_count):
-    for j in range(0, N, 2): #step = 2 since it's QPSK
-        bit1 = bits[i * 2 * N + j]
-        bit2 = bits[i * 2 * N + j + 1]
+def qpsk_modulate(data_bits):
+    symbols = []
+    for i in range(0, len(data_bits), 2):
+        bit_pair = data_bits[i : i + 2]
+        if np.array_equal(bit_pair, [0, 0]):    symbols.append(np.exp(1j * 0))       # 0 radians
         
-        if (bit1, bit2) == (0, 0): symbols[i, j // 2] = 1 + 1j #00
-
-        if (bit1, bit2) == (0, 1): symbols[i, j // 2] = -1 + 1j #01
+        elif np.array_equal(bit_pair, [0, 1]):  symbols.append(np.exp(1j * (np.pi / 2)))  # pi/2 radians
         
-        if (bit1, bit2) == (1, 1): symbols[i, j // 2] = -1 - 1j #11
+        elif np.array_equal(bit_pair, [1, 0]):  symbols.append(np.exp(1j * np.pi))   # pi radians
         
-        if (bit1, bit2) == (1, 0): symbols[i, j // 2] = 1 - 1j #10
-
-ifft_symbols = np.fft.ifft(symbols)
+        elif np.array_equal(bit_pair, [1, 1]):  symbols.append(np.exp(1j * (3 * np.pi / 2)))  # 3pi/2 radians
+        
+    return np.array(symbols)
 
 def AWGN(signal, signal_to_noise_ratio):
     target_noise_watts = 10 ** (signal_to_noise_ratio / 10)
@@ -31,19 +22,27 @@ def AWGN(signal, signal_to_noise_ratio):
     noise = np.sqrt(noise_power) * (np.random.normal(size = signal.shape) + 1j * np.random.normal(size = signal.shape))
     return signal + noise
 
-ifft_awgn_symbols = AWGN(ifft_symbols, signal_to_noise_ratio)
+N = 8 # amount of subcarriers
+symbols_count = 10
+bits_count = N * symbols_count * 2 #QPSK => *2
+signal_to_noise_ratio = 25
+data_bits = np.random.randint(0, 2, bits_count)
 
-received = np.fft.fft(ifft_awgn_symbols)
+# Модуляция QPSK
+qpsk_symbols = qpsk_modulate(data_bits)  * np.exp(1j * np.pi / 4) 
+ifft_symbols = np.fft.ifft(qpsk_symbols)
+awgn_symbols = AWGN(ifft_symbols, signal_to_noise_ratio)
+received = np.fft.fft(awgn_symbols)
+
 
 plt.figure(1)
 plt.subplot(2, 1, 1)
-plt.plot(np.real(ifft_symbols[0]), label = 'Re')
-plt.plot(np.imag(ifft_symbols[0]), label = 'Im')
+plt.plot(np.real(ifft_symbols), label = 'Re')
+plt.plot(np.imag(ifft_symbols), label = 'Im')
 plt.grid(True)
 plt.legend()
 
 plt.subplot(2, 1, 2)
-plt.scatter(np.real(received[0]), np.imag(received[0]))
+plt.scatter(np.real(received), np.imag(received))
 plt.grid()
-plt.show()       
-    
+plt.show()     
