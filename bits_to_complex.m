d6467bb943600af4cdfe2d1e2db43a7bb9a54140
Leo Gorenstein <152779643+ChaosAroundY0u@users.noreply.github.com>
2026-04-09 21:11:55 +0300

    encoded bits have output, encoded bits -> complex numbers

--- bits_to_complex.m
+++ bits_to_complex.m
@@ -1,8 +1,9 @@
-function out = bits_to_complex(symbs_count, nfft, num_subcarrier, modulation_technique)
+function [out, symbols] = bits_to_complex(symbs_count, nfft, num_subcarrier, modulation_technique)
     scale_var =                                     modulation_technique;
     x_count =                                       nfft * symbs_count * scale_var; % число бит
     x =                                             randi([0, 1], 1, x_count);
-    out =                                           zeros(1, symbs_count .* nfft);
+    out =                                           zeros(1, (symbs_count .* nfft) .*2);
+    symbols =                                       encoding(x, 0);
     switch scale_var
         case 1
             modulate =                              @bpsk_modulate;
@@ -13,10 +14,11 @@ function out = bits_to_complex(symbs_count, nfft, num_subcarrier, modulation_tec
         case 6
             modulate =                              @qam64_modulate;
     end
+    scale_var = scale_var .*2;
     for iterable = 1:symbs_count
-        x_use =                                     x(iterable*scale_var*nfft - scale_var*nfft + scale_var*num_subcarrier + 1:iterable*scale_var*nfft - scale_var * num_subcarrier);
+        x_use =                                     symbols(iterable*scale_var*nfft - scale_var*nfft + scale_var*num_subcarrier + 1:iterable*scale_var*nfft - scale_var * num_subcarrier);
         %symbol =                                    x(iterable*scale_var*nfft-scale_var*nfft+1:iterable*scale_var*nfft);
         modulated =                                 modulate(x_use);
-        out(iterable*nfft-nfft+1:iterable*nfft) =   [zeros(1, num_subcarrier), modulated, zeros(1, num_subcarrier)];
+        out(iterable.*nfft.*2-nfft.*2+1:iterable.*nfft.*2) =   [zeros(1, num_subcarrier.*2), modulated, zeros(1, num_subcarrier.*2)];
     end
 end
